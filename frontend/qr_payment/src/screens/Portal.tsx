fdaeba92f670913b5ce84fa778bb60e2078a0ea7
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2024-12-07 05:01:21 +0900

    トップ画面

--- frontend/qr_payment/src/screens/Portal.tsx
+++ frontend/qr_payment/src/screens/Portal.tsx
@@ -7,14 +7,17 @@ export const Portal = () => {
       <h2>トップ</h2>
       <ol>
         <li>
-          <LinkButton href="/scan-merchandise">商品購入ページ</LinkButton>
+          <LinkButton href="/scan-merchandise">商品購入</LinkButton>
         </li>
 
         <li>
-          <LinkButton href="/register-merchandise">商品登録ページ</LinkButton>
+          <LinkButton href="/register-merchandise">商品登録</LinkButton>
         </li>
         <li>
-          <LinkButton href="/merchandises">商品一覧ページ</LinkButton>
+          <LinkButton href="/merchandises">商品一覧</LinkButton>
+        </li>
+        <li>
+          <LinkButton href="/accounting">購入履歴</LinkButton>
         </li>
       </ol>
       <p>サーバーURL: {process.env.REACT_APP_BACKEND_URL}</p>
